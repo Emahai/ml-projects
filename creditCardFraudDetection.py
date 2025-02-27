3639dcff9d0a6178bdeab1e4533bd8c544f66445
Emanda Hailu <60546763+Emahai@users.noreply.github.com>
2025-02-27 22:00:05 +0300

    Update creditCardFraudDetection.py

--- creditCardFraudDetection.py
+++ creditCardFraudDetection.py
@@ -5,7 +5,6 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA, TruncatedSVD
 import matplotlib.patches as mpatches
 import time
